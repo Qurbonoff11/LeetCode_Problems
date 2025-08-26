6bf8ef7e3a80a6fe7fafb1f171ff6d06923ce0a8
Qurbonoff <jonibekqurbonov570@gmail.com>
2025-08-26 22:46:13 +0500

    edit 9-problem

--- 9/script.js
+++ 9/script.js
@@ -5,10 +5,13 @@ let arr = [1, 2, 3]
 
 const argumentsLength = (...args) => {
   let result = 0
-  for (let i = 0; i < args.length; i++) {
-    result += 1
-  }
+  // for (let i = 0; i < args.length; i++) {
+  //   result += 1
+  // }
+  // args.forEach(element => {
+  //   result += 1
+  // });
   return result
 }
 
-console.log(argumentsLength(num, str, bool, arr));
+console.log(argumentsLength(num, str));
