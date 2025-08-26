37fb7ba7cb0383427c1902bdfc92eaaeaeaf04e4
Qurbonoff <jonibekqurbonov570@gmail.com>
2025-08-26 17:25:47 +0500

    9-problem

---
+++ 9/script.js
@@ -0,0 +1,14 @@
+let num = 12
+let str = 'hello'
+let bool = true
+let arr = [1, 2, 3]
+
+const argumentsLength = (...args) => {
+  let result = 0
+  for (let i = 0; i < args.length; i++) {
+    result += 1
+  }
+  return result
+}
+
+console.log(argumentsLength(num, str, bool, arr));
