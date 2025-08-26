ae5a8141d3204e45b4c7f2176d063b44c928bbba
Qurbonoff <jonibekqurbonov570@gmail.com>
2025-08-26 17:40:38 +0500

    10-problem

---
+++ 10/script.js
@@ -0,0 +1,12 @@
+var once = function(fn) {
+
+    let obj = {calls: 1, val: fn}
+
+    return function(...args){
+        if(obj.calls === 1){
+            obj.val = fn(...args)
+            obj.calls = 0
+            return obj.val
+        }
+    }
+};
